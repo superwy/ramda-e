c3a80ec9c0b889a0ee0204ad0ca88518e83d0566
wangyang <superyfwy@gmail.com>
2018-01-18 01:13:25 +0800

    修改版本号

--- dist/ramda-e.js
+++ dist/ramda-e.js
@@ -16,19 +16,40 @@
  * @return {Boolean}
  * @example
  *
- *      RE.isPrice('0.12');       //=>  true
- *      RE.isPrice(10.1);      //=> true
+ *      RE.isInputPrice('0.12');       //=>  true
+ *      RE.isInputPrice(10.1);      //=> true
  */
-var isPrice = function isPrice(val) {
-  var reg = /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?$)/;
+var isInputPrice = function isInputPrice(val) {
+    var reg = /(^[1-9]([0-9]+)?(\.[0-9]{1,2})?$)|(^(0){1}$)|(^[0-9]\.[0-9]([0-9])?$)/;
   return reg.test("" + val);
+};
+
+/**
+ * Checks if value is `Integer`.
+ *
+ * @func
+ * @memberOf RE
+ * @since v0.0.1
+ * @category Math
+ * @sig * -> Boolean
+ * @param {*} val The value to test
+ * @return {Boolean}
+ * @example
+ *
+ *      RE.isInputInteger('11');       //=>  true
+ *      RE.isInputInteger(12);      //=> true
+ */
+var isInputInteger = function isInputInteger(val) {
+    var reg = /^(0|[1-9][0-9]*|-[1-9][0-9]*)$/;
+    return reg.test(val)
 };
 
 /**
  * @namespace RE
  */
 
-exports.isPrice = isPrice;
+exports.isInputPrice = isInputPrice;
+exports.isInputInterger = isInputInteger;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
